a64df525610b154acb7af7bc2a534fbec82261e3
vamsi-baipaneni <srivamsi.baipaneni@gmail.com>
2025-08-24 12:31:01 -0500

    changed to point to the correct key

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createBEClient() {
   const cookieStore = await cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
